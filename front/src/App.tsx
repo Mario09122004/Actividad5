777110e498af18304fa0519d4d05a98b6bdc2e3b
Mario09122004 <169079274+Mario09122004@users.noreply.github.com>
2025-06-25 19:57:03 -0600

    Rutas protegidas y JWT en localstorage

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -8,19 +8,22 @@ import './App.css';
 import AlumnosConsultar from "./screens/AlumnosConultar.tsx";
 import AlumnosModificar from "./screens/AlumnoModificar.tsx";
 import AlumnoEliminar from "./screens/AlumnoEliminar.tsx";
+import PrivateRoute from "./components/PrivateRoute.tsx";
 
 function App(){
     return (
         <div className="App">
             <BrowserRouter>
                 <Routes>
-                    {/* <Route path="/" element={<Login />} /> */}
-                    <Route path="/" element={<Menu />}>
-                        <Route index element={<ContenidoA />} />
-                        <Route path="agregar" element={<AlumonosAgregar />} />
-                        <Route path="consultar" element={<AlumnosConsultar />} />
-                        <Route path="modificar" element={<AlumnosModificar />} />
-                        <Route path="eliminar" element={<AlumnoEliminar />} />
+                    <Route path="/" element={<Login />} />
+                    <Route element={<PrivateRoute />}>
+                        <Route path="/alumnos" element={<Menu />}>
+                            <Route index element={<ContenidoA />} />
+                            <Route path="agregar" element={<AlumonosAgregar />} />
+                            <Route path="consultar" element={<AlumnosConsultar />} />
+                            <Route path="modificar" element={<AlumnosModificar />} />
+                            <Route path="eliminar" element={<AlumnoEliminar />} />
+                        </Route>
                     </Route>
                 </Routes>
             </BrowserRouter>
--- front/src/screens/Login.tsx
+++ front/src/screens/Login.tsx
@@ -1,67 +1,64 @@
-import { Outlet } from "react-router-dom";
-import { Button, Form, Input, Card, Typography, Row, Col } from 'antd';
-import { useNavigate } from 'react-router-dom';
 import React, { useRef, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { Button, Form, Input, Card, Typography, Row, Col } from 'antd';
 import ReCAPTCHA from 'react-google-recaptcha';
-//import { useAuth } from './../auth/AuthContex'
 
 const { Title } = Typography;
 
-function Login(){
-    
-  const [formData] = Form.useForm();
-  //const{ login } = useAuth();
+function Login() {
+  const [form] = Form.useForm();
   const navigate = useNavigate();
   const recaptchaRef = useRef(null);
   const [captchaValue, setCaptchaValue] = useState(null);
 
-  const handlerSubmit = async () => {
-  try {
-    const values = formData.getFieldsValue();
-
+  const handlerSubmit = async (values) => {
     if (!captchaValue) {
       alert("Por favor completa el CAPTCHA.");
       return;
     }
 
-    const response = await fetch('http://localhost:5000/api/auth/login-user', {
+    try {
+      const response = await fetch('http://localhost:5000/api/auth/login-user', {
         method: 'POST',
         headers: { 'Content-Type': "application/json" },
         body: JSON.stringify({
-            ...values,
-            captcha: captchaValue
+          ...values,
+          captcha: captchaValue
         })
-        });
-        recaptchaRef.current.reset();
+      });
 
-        const data = await response.json();
-        console.log('Respuesta del servidor: ', data);
+      recaptchaRef.current.reset();
 
-        if (data.success) {
-        // login(data.accessToken); // si usas autenticación con tokens
-        navigate('/');
-        formData.resetFields();
-        
-        } else {
-          alert(data.message || 'Inicio de sesión fallido');
-          formData.resetFields();
-        }
+      const data = await response.json();
+      console.log('Respuesta del servidor: ', data);
+
+      if (data.success) {
+        console.log('Inicio de sesión exitoso', data.token);
+        localStorage.setItem('token', data.token);
+
+        form.resetFields();
+        navigate('/alumnos');
+      } else {
+        alert(data.message || 'Inicio de sesión fallido');
+        form.resetFields();
+      }
 
     } catch (error) {
-        console.log('Error ocurrido en el login: ', error);
-        alert("Error al iniciar sesión");
+      console.error('Error ocurrido en el login: ', error);
+      alert("Error al iniciar sesión");
     }
-    };
+  };
 
   return (
     <Row justify="center" align="middle" style={{ minHeight: '100vh' }}>
       <Col xs={22} sm={16} md={12} lg={8}>
         <Card>
-          <Title level={2} style={{ textAlign: 'center', marginBottom: 24 }}>Iniciar de sesión</Title>
-          
+          <Title level={2} style={{ textAlign: 'center', marginBottom: 24 }}>Iniciar sesión</Title>
+
           <Form
-            form={formData}
+            form={form}
             layout="vertical"
+            onFinish={handlerSubmit}
           >
             <Form.Item
               label="Correo Electrónico"
@@ -87,8 +84,8 @@ function Login(){
               />
             </div>
 
-            <Form.Item style={{ marginBottom: 16 }}>
-              <Button type="primary" htmlType="submit" onClick={ handlerSubmit } block>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" block>
                 Iniciar Sesión
               </Button>
             </Form.Item>
@@ -97,6 +94,6 @@ function Login(){
       </Col>
     </Row>
   );
-};
+}
 
 export default Login;
